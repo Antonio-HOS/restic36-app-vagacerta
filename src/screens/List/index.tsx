8f3f3d4d98e28a9a0f44e4463423eafad2366e01
Antonio-HOS <ahosantos.cic@uesc.br>
2024-12-01 20:49:04 -0300

    fix: fetchbaga:/id

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,85 +1,78 @@
-import React, { useEffect } from 'react';
-import { Image, FlatList, View, Text } from 'react-native';
-import { Wrapper,Container, ListContainer, TextVagas } from './styles';
-import BGTop from '../../assets/BGTop.png';
-import Logo from '../../components/Logo';
-import VagaCard from '../../components/VagaCard';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import api from '../../services/api';
-
+import React, { useEffect } from "react";
+import { Image, FlatList, View, Text } from "react-native";
+import { Wrapper, Container, ListContainer, TextVagas } from "./styles";
+import BGTop from "../../assets/BGTop.png";
+import Logo from "../../components/Logo";
+import VagaCard from "../../components/VagaCard";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "../../services/api";
 
 export default function List() {
-    const [vagas, setVagas] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
-    
-    useEffect(() => {
-      const fetchVagas = async () => {
-          try {
-                const storedToken = await AsyncStorage.getItem('@token');
-                  if (storedToken) {
-                     api.defaults.headers.Authorization = `Bearer ${storedToken};`
-                   }
-                  const response = await api.get("/vagas",{
-                      headers: {
-                          Authorization: `Bearer ${storedToken}`
-                      }
-                  }); 
-                  setVagas(response.data.jobs);
-                  setLoading(false);
-                  console.log(response.data);
-              }
-          catch (error) {
-              console.log('Erro ao recuperar vagas:', error);
-              setLoading(false);
-          }
-      };
+  const [vagas, setVagas] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-      fetchVagas();
+  useEffect(() => {
+    const fetchVagas = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem("@token");
+        if (storedToken) {
+          api.defaults.headers.Authorization = `Bearer ${storedToken};`;
+        }
+        const response = await api.get("/vagas", {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        });
+        setVagas(response.data.jobs);
+        setLoading(false);
+        console.log(response.data);
+      } catch (error) {
+        console.log("Erro ao recuperar vagas:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchVagas();
   }, []);
-    if(loading){
-        return(
-            <View>
-                <Text>Carregando...</Text>
-            </View>
-        )
-    }
-    const DATA = vagas;
+  if (loading) {
     return (
-        <Wrapper>
-            <Image source={BGTop} style={{maxHeight: 86}}/>
-
-            <Container>
-
-                <Logo />
-                <TextVagas>{DATA.length} vagas encontradas!</TextVagas>
-                <ListContainer>
-                    <FlatList
-                        data={DATA}
-                        keyExtractor={(item) => item.id.toString()}
-                        renderItem={({item}) => 
-                            <VagaCard
-                                id={item.id}
-                                title={item.title} 
-                                dataCreated={item.dataCadastro}
-                                company={item.empresa}
-                            />
-                        }
-                        showsVerticalScrollIndicator={true}
-                        ListEmptyComponent={() => (
-                            <View>
-                                <Text>
-                                    Você ainda não tem tarefas cadastradas
-                                </Text>
-                                <Text>
-                                    Crie tarefas e organize seus itens a fazer.
-                                </Text>
-                            </View>
-                        )}
-                    />
-                </ListContainer>
-
-            </Container>
-        </Wrapper>
+      <View>
+        <Text>Carregando...</Text>
+      </View>
     );
+  }
+  const DATA = vagas;
+  return (
+    <Wrapper>
+      <Image source={BGTop} style={{ maxHeight: 86 }} />
+
+      <Container>
+        <Logo />
+        <TextVagas>{DATA.length} vagas encontradas!</TextVagas>
+        <ListContainer>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <VagaCard
+                id={item.id}
+                title={item.title}
+                dataCreated={item.dataCadastro}
+                company={item.empresa}
+              />
+            )}
+            showsVerticalScrollIndicator={true}
+            ListEmptyComponent={() => (
+              <View>
+                <Text>Você ainda não tem tarefas cadastradas</Text>
+                <Text>Crie tarefas e organize seus itens a fazer.</Text>
+              </View>
+            )}
+          />
+        </ListContainer>
+      
+      </Container>
+    </Wrapper>
+  );
 }

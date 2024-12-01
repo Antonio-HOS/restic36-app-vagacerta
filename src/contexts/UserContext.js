b748699b52906568c77b45fdfa3d7c5fb8651f8a
Antonio-HOS <ahosantos.cic@uesc.br>
2024-12-01 12:17:10 -0300

    create: adicionando contexto

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../services/api';
+import { Alert } from 'react-native';
+
+
+
+const UserContext = createContext(undefined);
+
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error('useUser deve ser usado dentro de um UserProvider');
+    }
+    return context;
+};
